629cb5813120697b286e52f4a8c346d30dbed739
sbrcly <sbrcly@gmail.com>
2024-11-23 10:32:28 -0800

    added footer

---
+++ components/MainFooter.jsx
@@ -0,0 +1,11 @@
+import classes from "./mainFooter.module.css"
+
+export default function MainFooter(params) {
+    return (
+        <footer className={classes.footer}>
+            <div className={classes["footer-content"]}>
+                <h2>Footer...</h2>
+            </div>
+        </footer>
+    )
+};
