afbf1b7d2282c803516dda16b17f3d5332305d66
sbrcly <sbrcly@gmail.com>
2024-11-24 14:55:21 -0800

    turn tiles into link and add hover effect

--- components/item-dropdown/Tile.jsx
+++ components/item-dropdown/Tile.jsx
@@ -1,17 +1,23 @@
 import Image from "next/image"
 
 import classes from "./tile.module.css"
+import Link from "next/link"
 
 export default function Tile({ tile }) {
     return (
         <div className={classes.tile}>
-            <Image
-                src={tile.image}
-                alt={tile.title}
-                width="300"
-                height="300"
-            />
-            <h2 className={classes.title}>{tile.title}</h2>
+            <Link href={tile.href}>
+                <div className={classes["image-wrapper"]}>
+                    <Image
+                        className={classes.image}
+                        src={tile.image}
+                        alt={tile.title}
+                        width="275"
+                        height="275"
+                    />
+                </div>
+                <h2 className={classes.title}>{tile.title}</h2>
+            </Link>
         </div>
     )
 };
--- data.js
+++ data.js
@@ -5,32 +5,38 @@ export const HEADER_OPTIONS = {
             {
                 id: '1',
                 title: "Pilot's Watches",
-                image: "/images/watches/pilots/pilot_hero.avif"
+                image: "/images/watches/pilots/pilot_hero.avif",
+                href: "/watches/pilot-watches"
             },
             {
                 id: '2',
                 title: "Portugieser",
-                image: "/images/watches/portugieser/portugieser_hero.avif"
+                image: "/images/watches/portugieser/portugieser_hero.avif",
+                href: "/watches/portugieser-watches"
             },
             {
                 id: '3',
                 title: "Portofino",
-                image: "/images/watches/portofino/portofino_hero.avif"
+                image: "/images/watches/portofino/portofino_hero.avif",
+                href: "/watches/portofino-watches"
             },
             {
                 id: '4',
                 title: "Ingenieur",
-                image: "/images/watches/ingenieur/ingenieur_hero.avif"
+                image: "/images/watches/ingenieur/ingenieur_hero.avif",
+                href: "/watches/ingenieur-watches"
             },
             {
                 id: '5',
                 title: "Aquatimer",
-                image: "/images/watches/aquatimer/aquatimer_hero.avif"
+                image: "/images/watches/aquatimer/aquatimer_hero.avif",
+                href: "/watches/aquatimer-watches"
             },
             {
                 id: '6',
                 title: "All Watches",
-                image: "/images/watches/all/all_watches_hero.avif"
+                image: "/images/watches/all/all_watches_hero.avif",
+                href: "/watches/all-watches"
             },
         ]
     },
@@ -40,17 +46,20 @@ export const HEADER_OPTIONS = {
             {
                 id: '1',
                 title: "Straps & Bracelets",
-                image: "/images/accessories/straps-bands/straps_bracelets_hero.avif"
+                image: "/images/accessories/straps-bands/straps_bracelets_hero.avif",
+                href: "/accessories/straps"
             },
             {
                 id: '2',
                 title: "Buckles & Clasps",
-                image: "/images/accessories/buckles-clasps/buckles_clasps_hero.avif"
+                image: "/images/accessories/buckles-clasps/buckles_clasps_hero.avif",
+                href: "/accessories/buckles"
             },
             {
                 id: '3',
                 title: "Watch Boxes",
-                image: "/images/accessories/boxes/boxes_hero.avif"
+                image: "/images/accessories/boxes/boxes_hero.avif",
+                href: "/accessories/boxes"
             },
         ]
     },
@@ -60,22 +69,26 @@ export const HEADER_OPTIONS = {
             {
                 id: '1',
                 title: "Service Types",
-                image: "/images/service/types/types_hero.avif"
+                image: "/images/service/types/types_hero.avif",
+                href: "/services/our-services"
             },
             {
                 id: '2',
                 title: "Service Price Calculator",
-                image: "/images/service/price/price_hero.avif"
+                image: "/images/service/price/price_hero.avif",
+                href: "/services/service-price-calculator"
             },
             {
                 id: '3',
                 title: "Care Advice",
-                image: "/images/service/advice/advice_hero.avif"
+                image: "/images/service/advice/advice_hero.avif",
+                href: "/services/care-advice"
             },
             {
                 id: '4',
                 title: "Extend Your Warranty",
-                image: "/images/service/warranty/warranty_hero.avif"
+                image: "/images/service/warranty/warranty_hero.avif",
+                href: "/services/my-iwc"
             },
         ]
     },
@@ -85,17 +98,20 @@ export const HEADER_OPTIONS = {
             {
                 id: '1',
                 title: "Find A Botique",
-                image: "/images/more/botique/botique_hero.webp"
+                image: "/images/more/botique/botique_hero.webp",
+                href: "/stores"
             },
             {
                 id: '2',
                 title: "Book An Appointment",
-                image: "/images/more/appointment/appointment_hero.webp"
+                image: "/images/more/appointment/appointment_hero.webp",
+                href: "/appointments"
             },
             {
                 id: '3',
                 title: "IWC Museum",
-                image: "/images/more/museum/museum_hero.avif"
+                image: "/images/more/museum/museum_hero.avif",
+                href: "/museum"
             }
         ]
     }
