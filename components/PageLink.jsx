e094c849e4f533e75b516636868f1ee5db95dc76
sbrcly <sbrcly@gmail.com>
2024-11-23 20:57:31 -0800

    various optimizations

--- components/PageLink.jsx
+++ components/PageLink.jsx
@@ -1,10 +1,26 @@
+"use client"
+
 import Link from "next/link"
 
 import classes from "./pageLink.module.css"
+import { useModalContext } from "@/contexts/modalContext"
 
 export default function PageLink({ title, href }) {
+    const { updateState } = useModalContext()
+
+    const handleClick = () => {
+        updateState({
+            isOpen: false,
+            current: '',
+            hold: false
+        })
+    }
+
     return (
-        <li className={classes.item}>
+        <li
+            onClick={handleClick}
+            className={classes.item}
+        >
             <Link href={href}>{title}</Link>
         </li>
     )
