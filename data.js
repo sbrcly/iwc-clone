73214dfaa34e9ebe555515303af6d374ac242473
sbrcly <sbrcly@gmail.com>
2024-11-23 22:04:37 -0800

    finish adding tiles to main header links

--- data.js
+++ data.js
@@ -33,5 +33,70 @@ export const HEADER_OPTIONS = {
                 image: "/images/watches/all/all_watches_hero.avif"
             },
         ]
+    },
+    accessories: {
+        title: "Accessories",
+        tiles: [
+            {
+                id: '1',
+                title: "Straps & Bracelets",
+                image: "/images/accessories/straps-bands/straps_bracelets_hero.avif"
+            },
+            {
+                id: '2',
+                title: "Buckles & Clasps",
+                image: "/images/accessories/buckles-clasps/buckles_clasps_hero.avif"
+            },
+            {
+                id: '3',
+                title: "Watch Boxes",
+                image: "/images/accessories/boxes/boxes_hero.avif"
+            },
+        ]
+    },
+    service: {
+        title: "Service",
+        tiles: [
+            {
+                id: '1',
+                title: "Service Types",
+                image: "/images/service/types/types_hero.avif"
+            },
+            {
+                id: '2',
+                title: "Service Price Calculator",
+                image: "/images/service/price/price_hero.avif"
+            },
+            {
+                id: '3',
+                title: "Care Advice",
+                image: "/images/service/advice/advice_hero.avif"
+            },
+            {
+                id: '4',
+                title: "Extend Your Warranty",
+                image: "/images/service/warranty/warranty_hero.avif"
+            },
+        ]
+    },
+    more: {
+        title: "Visit Us",
+        tiles: [
+            {
+                id: '1',
+                title: "Find A Botique",
+                image: "/images/more/botique/botique_hero.webp"
+            },
+            {
+                id: '2',
+                title: "Book An Appointment",
+                image: "/images/more/appointment/appointment_hero.webp"
+            },
+            {
+                id: '3',
+                title: "IWC Museum",
+                image: "/images/more/museum/museum_hero.avif"
+            }
+        ]
     }
 }
