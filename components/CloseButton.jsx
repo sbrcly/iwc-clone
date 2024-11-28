4b2a2a2deb0b9b286c76a7a32bbc9dc370d41ffd
sbrcly <sbrcly@gmail.com>
2024-11-28 11:44:46 -0800

    add search close button functionality

--- components/CloseButton.jsx
+++ components/CloseButton.jsx
@@ -1,8 +1,21 @@
+"use client"
+
 import classes from "./closeButton.module.css"
+import { useModalContext } from "@/contexts/modalContext"
 
 export default function CloseButton() {
+    const { updateState } = useModalContext()
+
+    const handleClick = () => {
+        updateState({
+            isOpen: false,
+            current: '',
+            hold: false
+        })
+    }
+
     return (
-        <button className={classes.close}>
+        <button onClick={handleClick} className={classes.close}>
             <span>Close</span>
             <span className={classes.x}>
                 <div className={classes["line-one"]}></div>
